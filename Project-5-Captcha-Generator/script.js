27fa0daf89c57750cb2cb699c8c9e3990211a116
Pratik Sutar <112809787+pratiksutar841@users.noreply.github.com>
2023-11-24 16:22:40 +0530

    Captcha-Generator

---
+++ Project-5-Captcha-Generator/script.js
@@ -0,0 +1,50 @@
+const display = document.getElementById("_status");
+const body = document.body;
+const submit = document.getElementById("_submit");
+const refresh = document.getElementById("_refresh");
+
+const char = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+let captcha = "";
+
+
+body.onload = function generateCaptcha(){
+
+    const captchaLength = 6;
+    for(let i = 0; i<captchaLength; i++){
+        const randomIndex = Math.floor(Math.random() * char.length);
+        captcha += char.substring(randomIndex,randomIndex + 1);
+    }
+
+    document.getElementById("_generator").value = captcha;
+    display.innerText = "Captcha Generator"
+
+
+}
+
+submit.onclick = function chekckInput(){
+    const input = document.getElementById("_client-text").value;
+
+    if(input === ""){
+        display.innerText = "Please Enter the text Shown below👇";
+    }else if(input === captcha){
+        display.innerText = "Matched😎";
+    }else{
+        display.innerText = "Not-Matched😖";
+    }
+
+}
+
+refresh.onclick = function refreshCaptcha(){
+
+    let newCaptcha = "";
+    for(let i = 0; i < captcha.length;i++){
+        const randomChar = char[Math.floor(Math.random() * char.length)]
+        newCaptcha += randomChar;
+    }
+    captcha = newCaptcha;
+    document.getElementById("_generator").value = captcha;
+    display.innerText = "Captcha Generator"
+
+
+}

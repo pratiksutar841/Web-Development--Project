e421775d264450809e4fef874eb0a318154b4433
Pratik Sutar <112809787+pratiksutar841@users.noreply.github.com>
2023-12-29 08:54:57 +0530

    Internet Status Checkar

---
+++ Project-38-Internet Status Checkar/script.js
@@ -0,0 +1,38 @@
+
+
+window.addEventListener("load",checkInternetConnection);
+
+function checkInternetConnection()
+{
+          const statusText = document.getElementById('statusText');
+          const ipAddressText = document.getElementById('ipAddressText');
+          const networkStrengthText = document.getElementById('networkStrengthText');
+          statusText.textContent= 'Checking. . . .';
+          if(navigator.onLine)
+          {
+                    fetch('https://api.ipify.org/?format=json')
+                    .then((response)=> response.json())
+                    .then((data)=>{
+                              ipAddressText.textContent=data.ip;
+                              statusText.textContent='connected'
+
+                              const connection = navigator.connection;
+
+                              const networkStrength= connection?connection.downlink
+                              +'Mbps':'Unknown';
+                              networkStrengthText.textContent=networkStrength;
+                    })
+                    .catch(()=>{
+                              statusText.textContent='Disconnected';
+                              ipaddressText.textContent='-'
+                              network.textContent='-'
+                    })
+
+          }
+          else{
+                    statusText.textContent ='Disconnected';
+                    ipaddressText.textContent ='-'
+          }
+
+
+}
